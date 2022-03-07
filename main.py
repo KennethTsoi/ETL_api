e0fabedb3b6651694f59511e4dac569148a1bdba
Kennytsoi <81626858+KennethTsoi@users.noreply.github.com>
2022-03-07 16:11:28 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import logging
+import json
+from flask import Flask, jsonify
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from raw_data import ORDERS_JSON
+
+FORMAT = "%Y%m%d_%H:%M:%S"
+
+logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO, datefmt="%H:%M:%S")
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+users = {"bpi": generate_password_hash("coe"), }
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+
+
+@app.route("/secret")
+@auth.login_required
+def secret():
+    return f"Hello {auth.current_user()}"
+
+
+@app.route("/")
+def main():
+    return "Welcome 💻"
+
+
+@app.route("/orders")
+@auth.login_required
+def orders():
+    return jsonify(ORDERS_JSON)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
